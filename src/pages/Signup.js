163ce33257024b279114e0d699340b9f8013feca
andreucc <andreucc@protonmail.com>
2019-03-31 17:52:21 +0200

    style logout

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -38,7 +38,7 @@ class Signup extends Component {
             <p>Signup</p>
           </div>
         </div>
-        <section>
+        {/* <section>
           <form onSubmit={this.handleFormSubmit}>
             <label>Username:</label>
             <input type="text" name="username" value={username} onChange={this.handleChange}/>
@@ -49,8 +49,30 @@ class Signup extends Component {
           <p>Already have account? 
             <Link to={"/login"}> Login</Link>
           </p>
-      </section>
+      </section> */}
+      <div className="row">
+        <section className="form-section">
+          <form className="user-form" onSubmit={this.handleFormSubmit}>
+            <div className="container-fields">
+              <div className="field">
+                <label>Username:</label>
+                <input type="text" name="username" value={username} onChange={this.handleChange} placeholder="username"/>
+              </div>
+              <div className="field">
+                <label>Password:</label>
+                <input type="password" name="password" value={password} onChange={this.handleChange} placeholder="password" />
+              </div>
+            </div>
+            <div className="buttons-bottom form">
+              <input className="btn btn-primary" type="submit" value="Login" />
+              <p>Already have account? 
+              <Link to={"/login"}> Login</Link>
+              </p>
+            </div>
+          </form>
+        </section>
       </div>
+    </div>
     )
   }
 }
