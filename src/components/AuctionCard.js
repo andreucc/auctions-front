fc520fdee1be9c53f9b933ebf25aeb44eef31ad7
xavivax1 <xavier.deviala@gmail.com>
2019-04-02 16:11:28 +0200

    cambios

--- src/components/AuctionCard.js
+++ src/components/AuctionCard.js
@@ -3,14 +3,15 @@ import {Link} from 'react-router-dom';
 import moment from 'moment';
 
 class AuctionCard extends Component {
-
-
+  
   render() {
+    console.log(this.props);
+    const Expiracion = moment(this.props.EndingTime).fromNow();
+
     return (
       <li className="card">
         <div className="img-container">
-          {/* <p>00:00:00</p> */}
-          <p>Expires in: {this.props.expiration}</p>
+          <p>Expires {Expiracion}</p>
           <Link to={`/auction/${this.props.id}`}><img src={this.props.image} alt="imgservice" /></Link>
           <h3>{this.props.name}</h3>
           <span className="current-price">{this.props.price}€</span>
--- src/components/FormCreateAuction.js
+++ src/components/FormCreateAuction.js
@@ -9,123 +9,131 @@ import { Link } from 'react-router-dom';
 
 
 class FormCreateAuction extends Component {
+  
+  
   state = {
     name: '',
     description: '',
     image: '',
     StartingPrice: '',
-    EndingDate: '', 
-    EndingTime: '',
+    EndingDate: '',
+    Time: '',
     status: true,
     isUploading: false,
     progress: 0
   }
 
+  componentDidMount() {
+    const now = moment();
+    const today = now.format('YYYY-MM-DD');
+    const maxDate = now.add(1, 'months').format('DD-MM-YYYY');
+    const defaultDate = now.add(3, 'days').format('YYYY-MM-DD');
+    const defaultTime = now.format('HH:mm:ss');
+    this.setState({
+      EndingDate: defaultDate,
+      Time: defaultTime
+    })
+  }
+  
   handleInput = (event) => {
     const { name, value } = event.target;
-    this.setState ({
-       [name]: value
+    this.setState({
+      [name]: value
     })
-    
+
   }
 
   handleCreate = (event) => {
     event.preventDefault();
-    
-    const {name, description, image, StartingPrice,EndingDate, EndingTime, status } = this.state;
-    const Ends = EndingDate + ' ' + EndingTime;
-    console.log('handleCreate...Ends:'+Ends+EndingDate+EndingTime);
-    const body = {name, description, image, StartingPrice, Ends, status };
-    console.log(body);      
-    apiService.createAuction(body);
+
+    const { name, description, image, StartingPrice, EndingDate, Time, status } = this.state;
+    const EndingTime = EndingDate + ' ' + Time;
+    const body = { name, description, image, StartingPrice, EndingTime, status };
+  
+
+    if (!name || !description || !StartingPrice || !EndingTime) {
+      // modal error
+    }
+    else {
+      apiService.createAuction(body);
+    }
   }
 
   handleUploadSuccess = (filename) => {
     console.log(firebase.storage().ref('autionImages'));
     this.setState({
-        image: filename,
-        progress: 100,
-        isUploading: false
+      image: filename,
+      progress: 100,
+      isUploading: false
     });
 
     firebase.storage().ref('auctionImages').child(filename)
-        .getDownloadURL()
-          .then(url => this.setState({ image: url }));
+      .getDownloadURL()
+      .then(url => this.setState({ image: url }));
   };
 
   render() {
 
-    //const {name, description, image, StartingPrice, EndingTime} = this.state;
-     
-    const {name, description, image, StartingPrice, EndingDate, EndingTime} = this.state;
-    // let {EndingDate, EndingTime} = this.state;
+    //const {name, description, image, StartingPrice, Time} = this.state;
+
+    const { name, description, image, StartingPrice, EndingDate, Time } = this.state;
     const now = moment();
     const today = now.format('YYYY-MM-DD');
     const maxDate = now.add(1, 'months').format('DD-MM-YYYY');
-    // EndingDate = now.add(3, 'days').format('YYYY-MM-DD');
-    // EndingTime= now.format('HH:mm:ss');
     const defaultDate = now.add(3, 'days').format('YYYY-MM-DD');
-    const defaultTime= now.format('HH:mm:ss');
+    const defaultTime = now.format('HH:mm:ss');
 
-      return (
-        <div>
-        <Navbar data='data'/>
-          <div className="row">
-            <section className="form-section">
-              <form className="user-form" onSubmit={this.handleCreate}>
-                <div className="container-fields auction">
-                    <img src={image} alt="activity" className="display-none"/>
-                    <div className="field">
-                      <label>Service image</label> 
-                      <FileUploader           
-                        accept="image/*"
-                        name="image"
-                        randomizeFilename
-                        storageRef={firebase.storage().ref('auctionImages')}
-                        onUploadStart={this.handleUploadStart}
-                        onUploadError={this.handleUploadError}
-                        onUploadSuccess={this.handleUploadSuccess}
-                        onProgress={this.handleProgress}
-                      />
-                    </div>
-                    <div className="field">
-                     <label>Service name</label>
-                      <input onChange={this.handleInput} type="text" name="name"  value={name} placeholder="Name"/>
-                    </div>
-                    <div className="field">  
-                      <label>Description</label>
-                      <input onChange={this.handleInput} type="text" name="description"  value={description} placeholder="Description"/>
-                    </div>
-                    <div className="field"> 
-                      <label>Starting price</label> 
-                      <input onChange={this.handleInput} type="number" name="StartingPrice"  value={StartingPrice} placeholder="StartingPrice"/>
-                    </div>
-                    <div className="field">
-                      {/* <label>Expiry time</label>
-                      <input onChange={this.handleInput} type="text" name="EndingTime"  value={EndingTime} placeholder="EndingTime"/> */}
-                      <label>End Date</label>
-                      <input type="date" defaultValue={defaultDate} min={today} max={maxDate} name="EndingDate"/>
-                      <label>End Time</label>
-                      <input type="time" defaultValue= {defaultTime} name="EndingTime"/>
-                    </div>
-                    {/*
-                    <div className="field last">
-                      <label>Status</label>  
-                       <input onChange={this.handleInput} type="text" name="status"  value={status} placeholder="Status"/>
-                    </div> 
-                    */}
+    return (
+      <div>
+        <Navbar data='data' />
+        <div className="row">
+          <section className="form-section">
+            <form className="user-form" onSubmit={this.handleCreate}>
+              <div className="container-fields auction">
+                <img src={image} alt="activity" className="display-none" />
+                <div className="field">
+                  <label>Service image</label>
+                  <FileUploader
+                    accept="image/*"
+                    name="image"
+                    randomizeFilename
+                    storageRef={firebase.storage().ref('auctionImages')}
+                    onUploadStart={this.handleUploadStart}
+                    onUploadError={this.handleUploadError}
+                    onUploadSuccess={this.handleUploadSuccess}
+                    onProgress={this.handleProgress}
+                  />
                 </div>
-                <div className="buttons-bottom">
-                  <button className="btn btn-primary" type="submit">Create Auction</button>
-                  {/* <Link to={"/auctions"}><button type="submit">Create Auction</button></Link> */}
+                <div className="field">
+                  <label>Service name</label>
+                  <input onChange={this.handleInput} type="text" name="name" value={name} placeholder="Name" />
                 </div>
-              </form>
-            </section>
-          </div>
+                <div className="field">
+                  <label>Description</label>
+                  <input onChange={this.handleInput} type="text" name="description" value={description} placeholder="Description" />
+                </div>
+                <div className="field">
+                  <label>Starting price</label>
+                  <input onChange={this.handleInput} type="number" name="StartingPrice" value={StartingPrice} placeholder="StartingPrice" min="1" />
+                </div>
+                <div className="field">
+                  <label>End Date</label>
+                  <input type="date" onChange={this.handleInput} defaultValue={defaultDate} min={today} max={maxDate} name="EndingDate" value={EndingDate} />
+                  <label>End Time</label>
+                  <input type="time" onChange={this.handleInput} defaultValue={defaultTime} name="Time" value={Time}/>
+                </div>
+              </div>
+              <div className="buttons-bottom">
+                <button className="btn btn-primary" type="submit">Create Auction</button>
+                {/* <Link to={"/auctions"}><button type="submit">Create Auction</button></Link> */}
+              </div>
+            </form>
+          </section>
         </div>
+      </div>
 
-      );
-         
+    );
+
   }
 }
 
